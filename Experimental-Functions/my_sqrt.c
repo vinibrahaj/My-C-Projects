61fe6f77c9053e1f4a09e688a7e4505326e78b84
Ervin Brahaj <ebrahaj@c1r5s2.tirana.42.school>
2025-11-29 10:16:58 +0100

    Experimental-Functions

---
+++ Experimental-Functions/my_sqrt.c
@@ -0,0 +1,94 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+    write (1, &c, 1);
+}
+
+int ft_putnbr(int nb)
+{
+    if (nb == -2147483648)
+    {
+        write(1, "-2147483648", 11);
+        return 0;
+    }
+
+    if (nb < 0)
+    {
+        write(1, "-", 1);
+        nb = -nb;
+    }
+    if (nb >= 10)
+    ft_putnbr(nb / 10);
+    ft_putchar(nb % 10 + '0');
+    return nb;
+}
+
+int ft_atoi(char *str)
+{
+    int result = 0;
+    int sign = 1;
+    int i = 0;
+
+    //Skip White space
+    while(str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+    {
+        i++;
+    }
+    //Handle sign
+    if (str[i] == '-' || str[i] == '+')
+    {
+        if(str[i] == '-')
+        {
+            sign = -1;
+        }
+        i++;
+    }
+    // Converting digits
+    while (str[i] >= '0' && str[i] <= '9')
+    {
+        result = result * 10 + (str[i] - '0');
+        i++;
+    }
+    return (sign * result);
+}
+
+int my_sqrt(int n)
+{
+    if (n < 0)
+    return 0;
+
+    if (n == 0 || n == 1) 
+    return n;
+
+    int i = 1;
+    while(i * i <= n)
+    {
+        i++;
+    }
+    return i - 1;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        write(1, "Error!\n(There need to be 2 arguments)\n", 38);
+        return 1;
+    }
+
+    int i = 0;
+
+    int number = ft_atoi(argv[1]);
+
+    int result = my_sqrt(number);
+
+    write (1, "Square root of ", 15);
+    ft_putnbr(number);
+    write (1, " is: ", 5);
+    ft_putnbr (result);
+    ft_putchar('\n');
+
+    return 0;
+
+}
